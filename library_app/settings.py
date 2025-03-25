0a71d4b3c059d7c3cb8a3d0abcdd9a98a5470105
nishy <ntsports18@gmail.com>
2025-03-25 20:46:34 +0900

    install dj_database_url

--- library_app/settings.py
+++ library_app/settings.py
@@ -78,7 +78,9 @@ WSGI_APPLICATION = 'library_app.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgres://localhost')
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL', default='postgres://localhost')
+                         )
 }
 
 
