d5b00c3896a81dc2aef103f07c95a6506d305a6b
nishy <ntsports18@gmail.com>
2025-03-30 23:55:39 +0900

    line and model created

--- books/views.py
+++ books/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-
 # Create your views here.
 import requests
 
@@ -7,6 +6,7 @@ from django.shortcuts import redirect
 from django.http import HttpResponse
 from django.conf import settings
 # Djangoのプラクティスで、settings.pyをインポートする
+from .models import User
 import logging
 
 # ロガーの設定
@@ -20,13 +20,14 @@ def line_login(request):
     auth_url = (
         "https://access.line.me/oauth2/v2.1/authorize?"
         f"response_type=code&client_id={settings.LINE_CHANNEL_ID}&"
-        f"redirect_uri={settings.LINE_REDIRECT_URI}&state=12345&scope=profile&ui_locales=ja"
+        f"redirect_uri={settings.LINE_REDIRECT_URI}&state=12345&scope=profile%20openid&ui_locales=ja"
     )
     # TODO:stateをランダムに生成
     
     return redirect(auth_url)
 
 def line_callback(request):
+    # LINE認証コールバック ライン側でcodeを取得をしている
     code = request.GET.get('code')
     if not code:
         return HttpResponse("認証エラー: codeがありません", status=400)
@@ -43,17 +44,38 @@ def line_callback(request):
     response = requests.post(token_url, data=data)
         # レスポンスの詳細をログに出力
     print(f"LINE APIレスポンス: {response.status_code}")
-    print(f"レスポンス内容: {response.text}")
+    # print(f"レスポンス内容: {response.text}")
     token_data = response.json()
 
-    if "access_token" not in token_data:
+    if "access_token" not in token_data or "id_token" not in token_data:
         return HttpResponse("トークン取得失敗", status=400)
     
 
+    # IDトークン検証
+    verify_url = "https://api.line.me/oauth2/v2.1/verify"
+    verify_data = {
+        "id_token": token_data["id_token"],
+        "client_id": settings.LINE_CHANNEL_ID,
+    }
+    verify_response = requests.post(verify_url, data=verify_data)
+    if verify_response.status_code != 200:
+        return HttpResponse(f"IDトークン検証失敗: {verify_response.json()}", status=400)
+    
+    
     # プロフィール取得
     profile_url = "https://api.line.me/v2/profile"
     headers = {"Authorization": f"Bearer {token_data['access_token']}"}
     profile = requests.get(profile_url, headers=headers).json()
 
-    # ログイン処理（仮に名前表示）
-    return HttpResponse(f"ようこそ、{profile['displayName']}さん！")
+    # Model
+    print(f"Profile: {profile}")  # ターミナル確認
+    try:
+        user, created = User.objects.get_or_create(
+            line_id=profile['userId'],
+            defaults={'name': profile['displayName']}
+        )
+        print(f"User saved: {user}, Created: {created}")#ログ出力
+        # ログイン処理（仮に名前表示）
+        return HttpResponse(f"ようこそ、{user.name}さん！")
+    except Exception as e:
+        return HttpResponse(f"エラー: {e}", status=400)
