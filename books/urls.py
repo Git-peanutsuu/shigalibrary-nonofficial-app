f937fbaf578d64761f83a0bb92f5c90790b69f28
nishy <ntsports18@gmail.com>
2025-05-26 01:59:17 +0900

    add logout

--- books/urls.py
+++ books/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('add/', views.add_book, name='add_book'),# ここだけだとエラー htmlもないし、views.pyもない
-    path('scan/', views.display_scan_page, name='display_scan_page'),
+    # path('scan/', views.display_scan_page, name='display_scan_page'),
     path('extract-isbn/', views.extract_isbn, name='extract_isbn'),
     path('search-book/', views.search_book_by_isbn, name='search_book_by_isbn'),
     path('search/', views.judge_search, name='search'),
